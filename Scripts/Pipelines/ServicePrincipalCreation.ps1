8f70c04e6731bfd075a7937d7968ebbfee492b47
Clark <chris.clark@dnvgl.com>
2020-03-26 11:52:41 +0000

    Updated assignment name

--- Scripts/Pipelines/ServicePrincipalCreation.ps1
+++ Scripts/Pipelines/ServicePrincipalCreation.ps1
@@ -1,7 +1,10 @@
+$pscredential = Get-Credential
+connect-AzAccount -ServicePrincipal -Tenant "adf10e2b-b6e9-41d6-be2f-c12bb566019c"
+
 # Get the reference to the resource group
 $resourceGroup = Get-AzResourceGroup -Name 'chrcla-rg'
 #Get assignment
-$assignment = Get-AzPolicyAssignment  -Id "/subscriptions/26a6717f-ee24-4802-bda7-0a17b0790911/resourceGroups/chrcla-RG/providers/Microsoft.Authorization/policyAssignments/DNV GL - Azure Batch - Testing RG"
+$assignment = Get-AzPolicyAssignment  -Id "/subscriptions/26a6717f-ee24-4802-bda7-0a17b0790911/resourceGroups/chrcla-RG/providers/Microsoft.Authorization/policyAssignments/Tagging"
 # Get the policy definition
 $policyDef = Get-AzPolicyDefinition -Id '/subscriptions/26a6717f-ee24-4802-bda7-0a17b0790911/providers/Microsoft.Authorization/policyDefinitions/Tagging'
 # Use the $policyDef to get to the roleDefinitionIds array
@@ -14,5 +17,6 @@ if ($roleDefinitionIds.Count -gt 0)
         New-AzRoleAssignment -Scope $assignment.Properties.Scope -ObjectId $assignment.Identity.principalId -RoleDefinitionId $roleDefId
     }
 }
+
 
 
